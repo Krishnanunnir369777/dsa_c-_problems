ed72de747d7c3fad58ba92eada1d3c7be0289e61
Krishnanunni <krishnanunnir503@gmail.com>
2024-10-22 21:50:26 +0530

    change book alocation file

--- Lecture 15 Binary/book_allocation.cpp
+++ Lecture 15 Binary/book_allocation.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
 using namespace std;
 # include <vector>
-int allocation(vector<int> arr,int n,int m){
+
+bool ispossible(vector<int> arr,int n,int m,int mid){
+    int studentCount=1;
+    int pagesum=0;
+    for(int i=0;i<n;i++){
+        if(pagesum+arr[i]<=mid){
+            pagesum+=arr[i];
+        }
+        else{
+            studentCount++;
+            if(studentCount>m || arr[i]>mid){
+                return false;
+            }
+            pagesum=arr[i];
+        }
+    }
+    return true;
+
+}
+int allocate(vector<int> arr, int n,int m){
     int s=0;
     int sum=0;
-    int e=n-1;
     for(int i=0;i<n;i++){
         sum+=arr[i];
     }
     int e=sum;
+    int ans=-1;
     int mid=s+(e-s)/2;
-    
-}
-
-int allocation(int arr,int n){
-    vector <int>arr;
-    for(int i=0;i<n;i++){
-        int right=right_sum(arr,n,i);
-        int left=left_sum(arr,n,i);
-        int maxi=max(right,left);
+    while(s<=e){
+        if(isPossible(arr,n,m,mid)){
+            ans=mid;
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+        mid=s+(e-s)/2;
     }
+    return ans;
 }
 
 int main(){
