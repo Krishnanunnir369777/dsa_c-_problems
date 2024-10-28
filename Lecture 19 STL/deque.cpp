785c0075983169e5045d79b92d0622f08197d574
Krishnanunni <krishnanunnir503@gmail.com>
2024-10-28 11:13:55 +0530

    some changes are made

---
+++ Lecture 19 STL/deque.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+#include <bits/stdc++.h>
+
+int main(){
+   deque<int>d;
+   d.push_back(1);
+   d.push_front(2);
+   
+   for(int i:d){
+    cout<<i<<endl;
+   }
+
+   //d.pop_back();
+   //cout<<"after pop"<<" ";
+   //for(int i:d){
+    //cout<<i<<endl;
+   //}
+
+   cout<<"first index element "<<d.at(0)<<endl;
+   cout<<d.front()<<endl;
+   cout<<d.back()<<endl;
+   cout<<d.size()<<endl;
+
+
+
+
+}
