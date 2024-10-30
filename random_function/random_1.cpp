2c292ba7889f794b0c5aad5db3dc37c5d233050b
Krishnanunni <krishnanunnir503@gmail.com>
2024-10-30 11:45:03 +0530

    new file addedd

--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -3,15 +3,29 @@
 #include <vector>
 using namespace std;
 
+void (int arr1[],int n,int arr2[],int m,int arr3[]){
+    int i=0,j=0;
+    int k=0;
+    while(i<n && j<m){
+        if(arr1[i]<arr2[j]){
+            arr3[k++]=arr1[i++];
+        }
+        else{
+            arr3[k++]=arr2[j++];
+        }
+    }
+    while(i<n){
+        
+    }
+}
 
 
 
 int main(){
-    cout<<"hello world" ;
+    int arr1[5]={1,3,5,7,9};
+    int arr2[3]={2,4,6};
+    int arr2[8]={0};
 
+    return 0;
 
-    
-   
-
- 
 }
