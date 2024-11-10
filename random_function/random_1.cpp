63e14385231a3d2e834b2794dc5a7cd0c56351d9
Krishnanunni <krishnanunnir503@gmail.com>
2024-11-10 23:13:14 +0530

    changes are made

--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -2,23 +2,21 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
-int count_prime(int n){
-    int cnt=0;
-    vector<bool> prime(n+1,true);
-
-    prime[0]=prime[1]=false;
-    
+int modular_exponentation(int x,int n,int m){
+    int res=1;
+    while(n>0){
+        if(n&1){
+            //odd
+            res=(1LL*(res) *(x)%m)%m;
+        }
+        x=(1LL*(x)%m *(x)%m)%m;
+        n=n>>1;
+    }
+    return res;
 }
+
 int main(){
-    int n;
-    cin>>n;
-    
-    cout<<count_prime(n);
-    
+   
 
-    
-
-    
     
 }
