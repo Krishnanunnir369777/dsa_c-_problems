21bb84537d7e2d9d67154f1a9c2c13df20af07c3
Krishnanunni <krishnanunnir503@gmail.com>
2024-11-16 18:07:08 +0530

    new file hello created

---
+++ Lecture 27 Double_Pointers/hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int i=5;
+    int *p=&i;
+    cout<<"printing p "<<p<<endl;
+    cout<<"address of p "<<&p<<endl;
+
+    int **p2=&p;
+    cout<<"address of p from p2  "<<p2<<endl;
+    cout<<"address of i from p2 "<<*p2<<endl;
+
+    cout<<**p2<<endl;
+
+
+}
