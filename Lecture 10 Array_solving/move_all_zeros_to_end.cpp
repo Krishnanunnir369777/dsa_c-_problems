f0e3101b5ee02a9f6c777ff98cc45fafb99aa6b2
Krishnanunni <krishnanunnir503@gmail.com>
2024-11-17 15:10:06 +0530

    create new functions

--- Lecture 10 Array_solving/move_all_zeros_to_end.cpp
+++ Lecture 10 Array_solving/move_all_zeros_to_end.cpp
@@ -1,12 +1,7 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-
-int main(){
-
-    int arr[3]={0,0,1};
-    int n=3;
-    int rot=0;
+#include <bits/stdc++.h>
+void my_func(int arr[],int n){
     for(int i=0;i<n-1;i++){
         rot=0;
         for(int j=0;j<n-1-i;j++){
@@ -24,4 +19,27 @@ int main(){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<endl;
     }
+
+
+}
+
+void better_approach(vector<int> &arr){
+
+    int count=0;
+
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]!=0){
+            swap(arr[i],arr[count++])
+      }
+    }
+    
+}
+
+
+int main(){
+    int arr[]={};
+    int n=;
+
+
+
 }
--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -3,27 +3,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int second_largest_element(vector<int> &arr){
-    int n=arr.size();
-    sort(arr.begin(),arr.end());
-
-    for(int i=n-2;i>=0;i--){
-        if(arr[i]!=arr[n-1]){
-            return arr[i];
-        }
-
-    }
-    return -1;
-
-}
 
 int main(){
-    //int arr[3]={3,2,1};
-    //int n=3;
-    //cout<<(largest_element(arr,n));
-    vector<int> arr={10,5,10};
-    cout<<second_largest_element(arr);
     
-    //cout<<arr.at();
+    
+
     
 }
