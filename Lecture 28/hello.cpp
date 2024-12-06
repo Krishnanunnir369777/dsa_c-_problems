710fec4c3890d9b3f7577c75e6a7641c1c1a5ed1
Krishnanunni <krishnanunnir503@gmail.com>
2024-12-06 15:54:58 +0530

    some changes are made

--- Lecture 28/hello.cpp
+++ Lecture 28/hello.cpp
@@ -4,7 +4,46 @@
 #include <vector>
 using namespace std;
 
+int& func(int a){// return by reference 
+
+    int num=a;
+    int& ans=num;
+
+    return ans;
+}
+void normal_update(int n){
+    n++;
+}
+void reference_variable_passing_update(int &n){
+    n++;
+}
 int main()
 {
-    cout<<2;
+    int i=2;
+
+    //creating a reference variable
+    
+    int &j=i;
+
+    cout<<i<<endl;
+    cout<<j<<endl;
+    i++;
+    cout<<i<<endl;
+    cout<<j<<endl;
+    j++;
+    cout<<i<<endl;
+    cout<<j<<endl;
+    
+   cout<<"before "<<i<<endl;
+   normal_update(i);
+   cout<<"after "<<i<<endl;
+   cout<<"before "<<i<<endl;
+   reference_variable_passing_update(i);
+   cout<<"after "<<i<<endl;
+   
+   cout<<"before "<<i<<endl;
+   int a=func(i);
+   cout<<"after "<<a<<endl;
+
+
 }
--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -7,9 +7,15 @@ using namespace std;
 
 
 int main(){
-    int n=5;
-    int *p=&n;
-    int **a=&p;
-    cout<<&p<<endl;
-    cout<<a<<endl;
+    int n;
+    cin>>n;
+    vector<int> arr;
+    int a=0;
+    for(int i=0;i<n;i++){
+        cin>>a;
+        arr.push_back(a);
+    }
+    for(int i :arr){
+        cout<<i<<endl;
+    }
 }
