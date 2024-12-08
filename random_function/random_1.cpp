a15bc3f17818aec4b80ae8b5c5a176fccfa8170c
Krishnanunni <krishnanunnir503@gmail.com>
2024-12-08 13:22:21 +0530

    some changes are made

--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -5,17 +5,6 @@ using namespace std;
 
 
 
-
 int main(){
-    int n;
-    cin>>n;
-    vector<int> arr;
-    int a=0;
-    for(int i=0;i<n;i++){
-        cin>>a;
-        arr.push_back(a);
-    }
-    for(int i :arr){
-        cout<<i<<endl;
-    }
+    
 }
