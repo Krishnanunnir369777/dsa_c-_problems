4a3b37667955603235e1cef078daa35d25bfe382
Krishnanunni <krishnanunnir503@gmail.com>
2024-12-22 11:42:33 +0530

    some changes

--- random_function/random_1.cpp
+++ random_function/random_1.cpp
@@ -4,7 +4,25 @@
 #include <cstdlib>
 using namespace std;
 #include <algorithm>
-int func
+
+void solve()
+vector <string> func(vector<vector<int>> &m, int n){
+
+    vector<string> ans;
+    if(m[0][0]==0){
+        return ans;
+    }
+    vector<vector<string>> visited=m;
+
+    for()
+
+    string path;
+    int srcx,srcy=0;
+    solve(m,n,ans,srcx,srcy,visited,path)
+}
+
+
+
 
 int main(){
     int a,b;
